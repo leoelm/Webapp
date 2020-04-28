1790e7ebebb375ae43fbd72f246a29bc4f79a807
leoelm <leo.elmecker@gmail.com>
2020-04-28 17:57:11 +0200

    web page changes

--- Javascript/index.js
+++ Javascript/index.js
@@ -28,9 +28,9 @@ window.onload = function() {
     }
 
     StartTextAnimation(0);
-    
+
     if(this.screen.width <= 767){
-        var menu = document.getElementById(button);
+        var menu = document.getElementById("button");
         menu.onclick = function() {
             menu.classList.toggle("is-acitve");
         }

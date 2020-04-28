03eea459a6b46977b572fa249be70f2256da0184
leoelm <leo.elmecker@gmail.com>
2020-04-28 15:48:44 +0200

    mobile navbar changes

--- Javascript/index.js
+++ Javascript/index.js
@@ -28,5 +28,11 @@ window.onload = function() {
     }
 
     StartTextAnimation(0);
+    if(this.screen.width <= 767){
+        var menu = document.getElementById(button);
+        menu.onclick = function() {
+            menu.classList.toggle("is-acitve");
+        }
+    }
 }
 
